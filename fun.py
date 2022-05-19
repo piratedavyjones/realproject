5f0573129b38ee7ae581e364b4b7fec326dabab4
Daniel Didam <gaggstheking007@gmail.com>
2022-05-19 16:34:54 +0100

    first commit

---
+++ fun.py
@@ -0,0 +1,6 @@
+students = ['john','mark','sam','james','sara','mary']
+if students == 'john':
+    print(f'Welcome {students} to the school')
+else:
+    print("You are not welcomed to the country")
+    
