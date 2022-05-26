765d7d010766f8246f9a24dcae4ae11d58f83ad5
Daniel Didam <gaggstheking007@gmail.com>
2022-05-26 16:13:29 +0100

    adding new

---
+++ assignment.py
@@ -0,0 +1,22 @@
+#tic tac toe
+
+board = [['-','-','-'],['-','-','-'],['-','-','-']]
+
+def display_board(board):
+    for row in board:
+        for space in row:
+            print(f"{space} ",end="" )
+        print()
+display_board(board) 
+
+def check_input(user_input):
+    #check if it's a number
+    #check if it's 1-9
+
+def isanum(user_input):
+
+
+while True:
+    
+    break
+
